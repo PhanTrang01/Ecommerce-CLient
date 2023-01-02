77247f6cd03cb572dd8a402f928194b9b16a1f8c
Dark-Star3110 <dangvan31102001@gmail.com>
2023-01-02 22:41:23 +0700

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import { useContext } from "react";
-import { UserContext } from "../contexts/UserContext";
-
 export default function Home() {
-  const { user } = useContext(UserContext);
-  return <>{user ? <h1>{user.name}</h1> : <h1>hello world</h1>}</>;
+  return (
+    <h1>Hello World</h1>
+  )
 }
