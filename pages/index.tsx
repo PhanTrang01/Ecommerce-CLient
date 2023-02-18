9bd6679fafab0df51d196e739e7bfd6cdf28310f
Dark-Star3110 <dangvan31102001@gmail.com>
2023-02-18 14:22:03 +0700

    add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import styled from "styled-components";
 import Sidebar from "../components/Sidebar";
 import ProductLayout from "../components/ProductLayout";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <Container>
-      <Sidebar />
-      <ProductLayout />
-    </Container>
+    <>
+      <Container>
+        <Sidebar />
+        <ProductLayout />
+      </Container>
+      <Footer />
+    </>
   );
 }
 
